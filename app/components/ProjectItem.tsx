6e31a1b0eda68de3ff0c369b3b29ae73054f711c
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2024-09-30 10:34:32 +0200

    border css

--- app/components/ProjectItem.tsx
+++ app/components/ProjectItem.tsx
@@ -11,11 +11,11 @@ interface ProjectItemProps {
 
 export default function ProjectItem({ displayLink, urlLink, imageSrc, title }: ProjectItemProps) {
     return (
-        <div className='relative flex flex-col h-[450px] sm:w-9/12 md:w-8/12 lg:w-7/12 xl:w-5/12 border bg-[#D9D9D9] bg-opacity-5 rounded-3xl mx-4 sm:mx-10'>
+        <div className='relative flex flex-col h-[450px] sm:w-9/12 md:w-8/12 lg:w-7/12 xl:w-5/12 border border-neutral-400 bg-[#D9D9D9] bg-opacity-5 rounded-3xl mx-4 sm:mx-10'>
             <div className='absolute inset-0 blur-[8rem] bg-[#61658F] -z-10'></div>
 
             <div className='p-4 flex flex-col'>
-                <a href={urlLink} target="_blank" rel="noopener noreferrer" className='border rounded-3xl px-8 py-2 bg-[#D9D9D9] bg-opacity-10 mb-4 w-fit'>
+                <a href={urlLink} target="_blank" rel="noopener noreferrer" className='border border-neutral-400 rounded-3xl px-8 py-2 bg-[#D9D9D9] bg-opacity-10 mb-4 w-fit'>
                     {displayLink}
                 </a>
                 <div className="relative w-full h-[280px] mx-auto">
