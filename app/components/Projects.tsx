b12393fb777c80c4ed739ef05218e8c6ad3cab40
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2024-09-30 08:17:12 +0200

    projets section responsive

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -4,9 +4,9 @@ import ProjectItem from './ProjectItem';
 export default function Projects() {
     return (
         <>
-            <section className="pt-40 w-9/12 mx-auto">
+            <section className="pt-40 w-full md:w-11/12 xl:w-10/12 2xl:w-9/12 mx-auto">
                 <h2 className="text-6xl font-medium leading-tight text-center">Projets</h2>
-                <div className="flex flex-wrap justify-between pt-28">
+                <div className="flex flex-col space-y-16 xl:space-y-0 xl:flex-row pt-28 items-center">
                     {projects.map((project, index) => (
                         <ProjectItem 
                             key={index}
